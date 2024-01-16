0742f9e9620a11ff20a8d85f89e50105180acbda
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2024-01-16 20:55:54 +0000

    Created basic page

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Componente versiones`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
